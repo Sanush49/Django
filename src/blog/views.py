b58ebb7ccecf03debd4b695a04d7857a5dd86159
Sanush Shakya <shakyasanush7@gmail.com>
2019-07-30 11:17:40 +0545

    Form added

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 # Create your views here.
 from .models import BlogPost
+from .forms import BlogPostModelForm
 
 #CRUD
 
@@ -22,8 +23,15 @@ def blog_post_list_view(request):
 def blog_post_create_view(request):
 	#create objects
 	# ? use a form
-	template_name='blog/create.html'
-	context={'form':None}
+	form = BlogPostModelForm(request.POST or None)
+	if form.is_valid():
+		print(form.cleaned_data)
+		obj = form.save(commit=False)
+		obj.title = form.cleaned_data.get("title") 
+		obj.save()
+		form = BlogPostModelForm()
+	template_name='form.html'
+	context={'form':form}
 	return render(request, template_name, context)
 
 def blog_post_detail_view(request,slug):
--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
+from .forms import ContactForm 
+
 def home_page(request):
 	my_title="Hello there..."
 	context ={"title":my_title,"list":[1,2,3,4,5]}
@@ -10,7 +12,15 @@ def about_page(request):
 	return render(request,"about.html",{"title": "About Us"})
 
 def contact_page(request):
-	return render(request,"contact.html",{"title": "Contact Us"})
+	form =ContactForm(request.POST or None)
+	if form.is_valid():
+		print(form.cleaned_data)
+		form = ContactForm()
+	context ={
+		"title": "Contact Us",
+		"form" : form
+	}
+	return render(request,"form.html",context)
 
 def example_page(request):
 	context = {"title":"Example"}
